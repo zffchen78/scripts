9dd6bb4c876c9c192b0f9722f09865a0eb1f4263
Zhifeng Chen <zhifengc@zhifengc-macbookair.roam.corp.google.com>
2014-07-12 23:33:46 -0700

    package archives.

--- elisp/init.el
+++ elisp/init.el
@@ -131,3 +131,16 @@
 )
 
 (startup)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives (quote (("marmalade" . "http://marmalade-repo.org/packages/") ("gnu" . "http://elpa.gnu.org/packages/")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
